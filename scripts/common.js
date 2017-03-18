56916a9284aead24ea9e2037b383dca7bc18460d
aleksandrsirenko <sirenko.alexandr.l@gmail.com>
2017-03-18 19:17:24 +0200

    changed files

--- scripts/common.js
+++ scripts/common.js
@@ -1,24 +1,55 @@
-﻿$('.my-panel').on('click', '.check', function () {
-    $(this).parents('.task-row').toggleClass('completed');
-})
+﻿
+taskService.getTasks();
+
+
+$('.my-panel').on('click', '.check', function () {
+    var taskRow = $(this).parents('.task-row');
+    taskRow.toggleClass('completed');
+    var task = {
+        id: taskRow.data('taskid'),
+        title: taskRow.find('span').text(),
+        isDone: $(this).prop('checked')
+    };
+    taskService.updateTask(task);
+});
 
 $('.my-panel').on('click', '.remove-icon', function () {
+    var taskId = $(this).parents('.task-row').data('taskid');
     $(this).parents('.task-row').remove();
-})
+    if (taskId != undefined) {
+        taskService.deleteTask(taskId);
+    }
+});
 
 
 $('#saveText').click(function () {
-    //var el = $('#addText').val();
-    var input1 = $('#addText');
-    var el = input1.val();
-    input1.val('');
-        if (el.trim() !== "") {
-            $('.panel-body').append('<div class="checkbox task-row"><button class="close" type="button"><i class="fa fa-times remove-icon" aria-hidden="true"></i></button><label><input type="checkbox" class="check" value="">' + el + '</label></div>');
-        } else {
-            return false;
-        }
-    })
+    var title = $('#addText').val();
+    if (title.trim() != '') {
+        var task = {
+            title: title
+        };
+        taskService.addTask(task);
+        $('#addText').val('');
+
+    } else {
+        showErrorMessage('Please enter a task title');
+    }
+})
+
     
 
+function buildTask(task) {
+    var container = $('.my-panel');
+    var el = '<div class="checkbox task-row' + (task.isDone ? ' completed' : '') + '" data-taskid=' + task.id + '><button class="close" type="button"><i class="fa fa-times remove-icon" aria-hidden="true"></i></button>'
+    el += '<label><input type="checkbox" class="check" value=""><span>' + task.title + '</span></label></div>';
 
+    container.append(el);
+}
 
+function showErrorMessage(message) {
+    var input = $('#newTask input');
+    var el = $('<p></p>');
+    el.addClass('error-message');
+    el.text(message);
+    input.after(el);
+}
---
+++ scripts/taskService.js
@@ -0,0 +1,66 @@
+﻿var taskService = (function () {
+    return {
+        baseUrl: 'http://learn-todo.gear.host/api/tasks',
+        getTasks: function () {
+            var self = this;
+            $.ajax({
+                url: self.baseUrl,
+                type: 'GET',
+                success: function (response) {
+                    console.log('Success', response);
+                    for (var i = 0; i < response.length; i++) {
+                        buildTask(response[i]);
+                    }
+                    $('.task-row.completed').find('.check').prop('checked', true);
+                },
+                error: function (response) {
+                    console.log('Error', response);
+                }
+            })
+        },
+        addTask: function (task) {
+            var self = this;
+            $.ajax({
+                url: self.baseUrl,
+                type: 'POST',
+                data: task,
+                success: function (response) {
+                    console.log('Success', response);
+                    buildTask(response);
+                },
+                error: function (response) {
+                    console.log('Error', response);
+                }
+            })
+        },
+
+        deleteTask: function (taskId) {
+            var self = this;
+            $.ajax({
+                url: self.baseUrl + '/' + taskId,
+                type: 'DELETE',
+                success: function (response) {
+                    console.log('Success', response);
+                },
+                error: function (response) {
+                    console.log('Error', response);
+                }
+            })
+        },
+        updateTask: function (task) {
+            var self = this;
+            $.ajax({
+                url: self.baseUrl,
+                type: 'PUT',
+                data: task,
+                success: function (response) {
+                    console.log('success', response);
+                },
+                error: function (response) {
+                    console.log('error', response);
+                }
+            })
+        }
+
+    }
+})();
