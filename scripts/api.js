5af3b6c581684fd1e86435bac27ffbc04055ce5f
aleksandrsirenko <sirenko.alexandr.l@gmail.com>
2017-04-12 18:27:48 +0300

    added ajax query

---
+++ scripts/api.js
@@ -0,0 +1,40 @@
+﻿var route = (function() {
+    return {
+        getRegister: function () {
+            $.ajax({
+                url: '/registerForm.html',
+                type: 'GET',
+                success: function(data) {
+                    $('#main').html(data);
+                },
+                error: function(data) {
+                    console.log('Error getRegister', data);
+                }
+            });
+        },
+        getLogin: function() {
+            $.ajax({
+                url: '/loginForm.html',
+                type: 'GET',
+                success: function(data) {
+                    $('#main').html(data);
+                },
+                error: function(data) {
+                    console.log('Error getLogin', data);
+                }
+            });
+        },
+        getTasks: function() {
+            $.ajax({
+                url: '/tasks.html',
+                type: 'GET',
+                success: function(data) {
+                    $('#main').html(data);
+                },
+                error: function(data) {
+                    console.log('Error getTasks', data);
+                }
+            });
+        }
+    }
+})();
